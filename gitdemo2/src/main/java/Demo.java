1a9df995b35c432924725dfb2a343519a6f85762
xiaobai <747575798@qq.com>
2021-08-13 13:59:03 +0800

    徐康

--- gitdemo2/src/main/java/Demo.java
+++ gitdemo2/src/main/java/Demo.java
@@ -1,2 +1,9 @@
 public class Demo {
+    public void add(){
+        System.out.println("小白添加的");
+        System.out.println("徐康添加的");
+    }
+    public void add2(){
+        System.out.println("XX添加的");
+    }
 }
